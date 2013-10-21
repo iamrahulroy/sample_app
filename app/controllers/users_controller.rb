8caa6b2ff1c579ab04e885260bf99642c6deee94
Rahul <techniciablog@gmail.com>
2013-10-21 23:49:41 +0530

    Signup failure: Added basic validation and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,15 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user]) # Not the final implementiona!
+  	@user = User.new(user_params)
   	if @user.save
   		# Handle a successful save.
   	else
   		render 'new'
   	end
   end
+
+  def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 end
