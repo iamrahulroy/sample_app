3ea14c660a64899c17ba957b23445c8e093e25ed
Rahul <techniciablog@gmail.com>
2013-10-24 15:10:28 +0530

    Completed sessions and signin failure

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,13 +12,19 @@ describe "Authentication" do
   end
 
   describe "signin" do
+
   	before { visit signin_path }
 
   	describe "with invalid information" do
-  		before { click_button "Singin in" }
+  		before { click_button "Sign in" }
 
   		it { should have_title('Sign in') }
   		it { should have_selector('div.alert.alert-error', text: "Invalid") }
+
+  		describe "after visiting another page" do
+  			before { click_link "Home" }
+  			it { should_not have_selector('div.alert.alert-error') }
+  		end
   	end
   end
 
