dffd98c7557c549a460a734f4648d51dbaf7ba80
Rahul <techniciablog@gmail.com>
2013-10-31 00:49:56 +0530

    Added users index page and other related functionalities

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "Authentication" do
 
 	subject { page }
+  # let(:user) { FactoryGirl.create(:user) }
 
   describe "signin page" do
   	before { visit signin_path }
@@ -30,14 +31,16 @@ describe "Authentication" do
 
   describe "with valid information" do
   	let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user }
  
-  	before do
-  		fill_in "Email", with: user.email.upcase
-  		fill_in "Password", with: user.password
-  		click_button "Sign in"
-  	
+  	# before do
+  	# 	fill_in "Email", with: user.email.upcase
+  	# 	fill_in "Password", with: user.password
+  	# 	click_button "Sign in"
+  	# end
 
   	it { should have_title(user.name) }
+    it { should have_link('Users', href: users_path) }
   	it { should have_link('Profile', href: user_path(user)) }
     it { should have_link('Settings', href: edit_user_path(user)) }
   	it { should have_link('Sign out', href: signout_path) }
@@ -47,7 +50,7 @@ describe "Authentication" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
-    end
+    # end
   end
 
   describe "authorization" do
@@ -70,18 +73,23 @@ describe "Authentication" do
           end
         end
       end
+    end
 
-      describe "in the Users controller" do
+    describe "in the Users controller" do
 
-        describe "visit the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_title('Sign in') }
-        end
+      describe "visit the edit page" do
+        before { visit edit_user_path(user) }
+        it { should have_title('Sign in') }
+      end
 
-        describe "submitting to the update action" do
-          before { patch user_path(user) }
-          specify { expect(response).to redirect_to(signin_path) }
-        end
+      describe "submitting to the update action" do
+        before { patch user_path(user) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+
+      describe "visiting the user index" do
+        before { visit users_path }
+        it { should have_title('Sign in') }
       end
     end
 
@@ -90,13 +98,13 @@ describe "Authentication" do
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user, no_capybara: true }
 
-      describe "submitting a GET request to the User#edit action" do
+      describe "submitting a GET request to the Users#edit action" do
         before { get edit_user_path(wrong_user) }
         specify { expect(response.body).not_to match(full_title('Edit user'))} 
         specify { expect(response).to redirect_to(root_url) }
       end
 
-      describe "submitting a PATCH request to the User#update action" do
+      describe "submitting a PATCH request to the Users#update action" do
         before { patch user_path(wrong_user) }
         specify { expect(response).to redirect_to(root_url) }
       end
