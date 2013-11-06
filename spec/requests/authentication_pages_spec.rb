06ac1a871b5a580836edc8f108d265a30028838e
Rahul <techniciablog@gmail.com>
2013-11-07 00:46:29 +0530

    Manipulating microposts: Access control

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -73,6 +73,20 @@ describe "Authentication" do
           end
         end
       end
+
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+            specify {  expect(response).to redirect_to(signin_path) }
+          end
+        end
+      # end
     end
 
     describe "in the Users controller" do
@@ -109,17 +123,18 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
-  end
+  
 
-  describe "as non-admin user" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:non_admin) { FactoryGirl.create(:user) }
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
 
-    before { sign_in non_admin, no_capybara: true }
+      before { sign_in non_admin, no_capybara: true }
 
-    describe "submitting a DELETE request to the Users#destroy action" do
-      before { delete user_path(user) }
-      specify { expect(response).to redirect_to(root_url) }
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
     end
   end
 end
